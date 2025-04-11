2ba3a6e1c17f0625552f83db2ac7d00c9039d00c
Sumedh Charjan <sumedhcharjan321@gmail.com>
2025-04-11 17:09:57 +0530

    force push to main

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const landingPage = () => {
+  return (
+    <>
+      <div class="color-green font-bold">landingPage sjafjdskljfklasjfkjsf</div>
+      <h1 class="text-3xl font-bold underline"> Hello world! </h1>
+    </>
+  )
+}
+
+export default landingPage
