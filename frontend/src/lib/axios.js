e5f1046ba34d8e9585df0a15c25ee416ceb4f3db
kalpm1110 <kalpm1832@gmail.com>
2025-08-05 17:59:32 +0530

    UPDATED

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,11 @@
+// import axios from "axios";
+// const instance=axios.create({
+//     baseURL:'https://collabboard-8o88.onrender.com/api',withCredentials:true
+// })
+// export default instance;
+
 import axios from "axios";
 const instance=axios.create({
-    baseURL:'https://collabboard-8o88.onrender.com/api',withCredentials:true
+    baseURL:'http://localhost:8080/api',withCredentials:true
 })
 export default instance;
--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io('https://collabboard-8o88.onrender.com', {
+const socket = io('http://localhost:8080', {
     transports: ['websocket'],
     withCredentials: true,
 });
