4189f53971a8c982b5b89123a3f83151f2256b94
sumedhcharjan <sumedhcharjan321@gmail.com>
2025-08-06 18:05:34 +0530

    Update socket.js

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,5 +1,11 @@
+// import { io } from 'socket.io-client';
+// const socket = io('http://localhost:8080', {
+//     transports: ['websocket'],
+//     withCredentials: true,
+// });
+// export default socket;
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:8080', {
+const socket = io('https://collabboard-8o88.onrender.com', {
     transports: ['websocket'],
     withCredentials: true,
 });
