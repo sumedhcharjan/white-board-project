8eec8a6f6160b2d2a1ea077657dd3650ced0610c
sumedhcharjan <sumedhcharjan321@gmail.com>
2025-08-06 18:04:50 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,11 @@
-// import axios from "axios";
-// const instance=axios.create({
-//     baseURL:'https://collabboard-8o88.onrender.com/api',withCredentials:true
-// })
-// export default instance;
-
 import axios from "axios";
 const instance=axios.create({
-    baseURL:'http://localhost:8080/api',withCredentials:true
+    baseURL:'https://collabboard-8o88.onrender.com/api',withCredentials:true
 })
 export default instance;
+
+// import axios from "axios";
+// const instance=axios.create({
+//     baseURL:'http://localhost:8080/api',withCredentials:true
+// })
+// export default instance;
